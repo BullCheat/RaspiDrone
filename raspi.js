3a90b4ec9e4ab2e43ece81e0605f1590b6e81e15
pcraft@outlook.fr <pcraft@outlook.fr>
2016-10-30 19:15:01 +0100

    Fault tolerant

--- raspi.js
+++ raspi.js
@@ -1,14 +1,32 @@
 require('./common.js');
-var client = new global.net.Socket();
-client.connect(PORT_GENERAL, DESKTOP_IP, function() {
-  console.log("GENERAL TCP " + PORT_GENERAL + " CONNECTED");
-  client.write("Hello world!");
-});
 
-client.on('data', function(data) {
-  console.log('GENERAL TCP ' + PORT_GENERAL + ' - ' + data);
-});
+var general = {failed:0};
 
-client.on('close', function() {
-  console.log('GENERAL TCP ' + PORT_GENERAL + " DISCONNECTED");
-});
+function connectGeneral() {
+
+  var client = new global.net.Socket();
+  client.connect(PORT_GENERAL, DESKTOP_IP, function() {
+    console.log("GENERAL TCP " + PORT_GENERAL + " CONNECTED");
+    client.write("Hello world!");
+    general.failed = 0;
+  });
+
+  client.on('data', function(data) {
+    console.log('GENERAL TCP ' + PORT_GENERAL + ' - ' + data);
+  });
+
+  client.on('close', function() {
+    if (general.failed < 2) {
+      console.log('GENERAL TCP ' + PORT_GENERAL + " DISCONNECTED");
+      console.log('Trying to reconnect');
+    }
+    connectGeneral();
+  });
+
+  client.on('error', function(e) {
+    if (general.failed < 2) console.log(e);
+    general.failed++;
+  });
+}
+
+connectGeneral();
