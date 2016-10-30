f4e1b86eb5e673a4d30dbf2a9a977528267a017d
pcraft@outlook.fr <pcraft@outlook.fr>
2016-10-30 19:05:09 +0100

    Hello-World

---
+++ desktop.js
@@ -0,0 +1,17 @@
+require('./common.js');
+
+var server = net.createServer(function(socket) {
+  console.log("GENERAL TCP " + PORT_GENERAL + " CONNECTED");
+  socket.write("Hello world!\r\n");
+  socket.on("data", function(data) {
+    console.log("GENERAL TCP " + PORT_GENERAL + " - " + data);
+  });
+});
+
+server.on('error', (err) => {
+  console.log("GENERAL TCP " + PORT_GENERAL + " ===== " + err);
+});
+
+server.listen(PORT_GENERAL, '0.0.0.0', () => {
+  console.log("GENERAL TCP " + PORT_GENERAL + " BOUND");
+});
