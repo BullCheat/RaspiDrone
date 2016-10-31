e7bd863fdb4c6bffba4ecc1f67c4434b1e0a626a
pcraft@outlook.fr <pcraft@outlook.fr>
2016-10-31 14:57:06 +0100

    startsWith

--- common.js
+++ common.js
@@ -4,3 +4,10 @@ global.DESKTOP_IP = '88.182.38.228';
 global.PORT_GENERAL = 1000;
 
 global.net = require('net');
+
+if (typeof String.prototype.startsWith != 'function') {
+  // see below for better implementation!
+  String.prototype.startsWith = function (str){
+    return this.indexOf(str) === 0;
+  };
+}
