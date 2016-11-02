aa33973374d7aa0897b5dd17fba311414430b366
pcraft@outlook.fr <pcraft@outlook.fr>
2016-11-02 18:30:46 +0100

    Fonctionnait

--- desktop.js
+++ desktop.js
@@ -1,31 +1,20 @@
 require('./common.js');
-var carrier = require('carrier');
-var BufferBuilder = require('buffer-builder');
 var io = require('socket.io').listen(1050);
 
-var multiDevices = [];
-
-var multicast = net.createServer(function(socket) {
-  multiDevices.push(socket);
-  socket.setNoDelay(true);
-  socket.on('data', function(data) {
-    multiDevices.forEach((client) => {
-      if (client === socket || client.destroyed) return;
-      client.write(data);
-      console.log(data);
-    });
-  });
-}).listen(1002);
+io.on('connection', (socket) => {
+  socket.on('log', console.log);
+});
 
 var server = net.createServer(function(socket) {
-  console.log("GENERAL TCP " + PORT_GENERAL + " CONNECTED");
+  console.log("GENERAL TCP " + PORT_GENERAL + " CONNECTED " + socket.remoteAddress);
   global.socket_general = socket;
-  //socket.setTimeout(2000);
+  socket.setTimeout(2000);
+  socket.setKeepAlive(true);
   carrier.carry(socket, function(line) {
     console.log("GENERAL TCP " + PORT_GENERAL + " - " + line);
   });
   socket.setNoDelay(true);
-  socket.write("Hello world!");
+  //socket.write("Hello world!");
   socket.on('error', (err) => {
     console.log("GENERAL TCP " + PORT_GENERAL + " ===== " + err);
   });
@@ -48,18 +37,9 @@ var c = (x) => {
 };
 io.on("connection", (socket) => {
   socket.on("trame", (trame) => {
-    if (typeof device !== 'undefined' && device) {
+    if (typeof socket_general !== 'undefined' && socket_general && !socket_general.destroyed) {
       //socket_general.write("trame" + JSON.stringify([parseInt(500*(4+trame.throttle)), parseInt(500*(4+trame.ailerons)), parseInt(500*(4+trame.profondeur)), parseInt(500*(4+trame.switch)), parseInt(500*(4+trame.molette))]));
-      device.setRawRc({roll: c(trame.ailerons),pitch:c(trame.profondeur),yaw:c(0),throttle:c(trame.throttle),aux1:c(trame.switch),aux2:c(trame.molette),aux3:c(0),aux4:c(0)}, {prior:true}, function(e,r) {});
+      socket_general.write("trame" + JSON.stringify({roll: c(trame.ailerons),pitch:c(trame.profondeur),yaw:c(0),throttle:c(trame.throttle),aux1:c(trame.switch),aux2:c(trame.molette),aux3:c(0),aux4:c(0)}) + '\n');
     }
   });
-});
-
-var TcpServer = require('multiwii-msp').TcpServer;
-var tserver = new TcpServer(1001, true);
-tserver.on('register', (key,device) => {
-  device.on('open', () => {
-  });
-  device.on('update', (u) => {console.log(u); global.device = device;});
-  device.on('close', () => {});
 });
