97a07b7559b2dd01ca09ba7e4ee52bcad733fd83
pcraft@outlook.fr <pcraft@outlook.fr>
2016-11-04 18:32:25 +0100

    Common

--- common.js
+++ common.js
@@ -4,6 +4,9 @@ global.DESKTOP_IP = '88.182.38.228';
 global.PORT_GENERAL = 1000;
 
 global.net = require('net');
+global.carrier = require('carrier');
+global.msp = require('msp');
+global.protocol = new msp.Protocol();
 
 if (typeof String.prototype.startsWith != 'function') {
   // see below for better implementation!
